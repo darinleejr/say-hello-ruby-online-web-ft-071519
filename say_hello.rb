e3f0e985f092d0ad3997d7e09ea5ccbf30b3032e
Darin Webster <darinleenavy@gmail.com>
2019-07-19 18:14:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,14 @@
 # Build your say_hello method here
+
+def say_hello(name)
+  name = "Kent Beck"
+  puts "Hello #{name}!"
+end
+
+def say_hello(name)
+  name == name
+  while name < 1
+  puts "Hello #{name}!"
+  end
+end
+  
